8ca1cdd19e2f26b9d87d30f0c5abea98b39a4357
+ <+>
2023-03-15 00:40:02 +0300

    Arrow Functions

---
+++ index.js
@@ -0,0 +1,17 @@
+//  has a function expression called divide:
+function divide(x ,y ) {
+    x= 2000;
+    y= 100;
+
+    let a=x/y
+
+    return a;
+  }
+
+  //has an arrow function called square
+
+  let square = num =>  num*num;
+  
+//has an arrow function called add:
+
+   let add =(num1, num2 ) => num1+num2;
